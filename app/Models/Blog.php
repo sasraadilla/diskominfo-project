51a0072b0d1f5c5c0680bc89390d8951d346ccd2
sasraadilla <sasraadilla30@gmail.com>
2024-08-21 18:02:53 +0700

    first commit

---
+++ app/Models/Blog.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Blog extends Model
+{
+    protected $fillable = [
+        'title',
+        'content',
+        'image',
+        'category_id',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
